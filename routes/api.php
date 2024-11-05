0cf6f8bf3ec4d266aad2a6eec0a75e258e12518a
Narvana <adhikarijagdish3@gmail.com>
2024-11-05 11:02:34 +0530

    api.php Admin authentication api added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Users\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users\UsersAccessController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware'=>['api'],'prefix'=>'auth'],
+function()
+{
+
+    Route::get('/Test',[TestController::class,'Test']);
+
+    Route::post('/Admin/Register',[UsersAccessController::class,'AdminRegister']);
+
+    Route::post('/Admin/Login',[UsersAccessController::class,'AdminLogin']);
+});
+
